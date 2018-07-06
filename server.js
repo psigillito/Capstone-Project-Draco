5dc0b5d04e136c446810a81a11903a86968461f3
Nick DeJarnette <nick.dejarnette@gmail.com>
2018-07-06 00:45:36 -0400

    Updated initial server structure

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 // Bring in modules
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 // Initialize app
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport middleware
+app.use(passport.initialize());
 
 // Test route - DELETE ME LATER
 app.get('/', (req, res) => {
 	res.send('This is a test');
 });
 
+// Port setting for later deployment to Heroku
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
