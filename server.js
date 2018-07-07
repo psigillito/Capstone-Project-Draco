5362b1ba142b7a6f1e71393b6d1f75e910dae0a6
Chris Capps <capps.christopher@gmail.com>
2018-07-07 09:22:23 -0400

    Heroku configuration

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+const dotenv = require("dotenv").config();
 
 // Initialize app
 const app = express();
@@ -14,6 +16,12 @@ app.use(bodyParser.json());
 // Passport middleware
 app.use(passport.initialize());
 
+// React build middleware
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// Connect to the db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/")  
+
 // Test route - DELETE ME LATER
 app.get('/', (req, res) => {
 	res.send('This is a test');
@@ -21,5 +29,10 @@ app.get('/', (req, res) => {
 
 // Port setting for later deployment to Heroku
 const port = process.env.PORT || 5000;
+const secret = process.env.SECRET || "some secret passphrase here for local development"
+
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
