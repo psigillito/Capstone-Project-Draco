328152dc799725ce419475f1a85122e7a53d5c7b
Chris Capps <capps.christopher@gmail.com>
2018-07-12 21:44:13 -0400

    POST to /workouts now works

--- routes/workouts.js
+++ routes/workouts.js
@@ -8,28 +8,38 @@ const Workout = require('../models/workouts');
 // public access
 
 router.get('/', (req, res) => {
-    var query = Workout.find();
-    query.exec((err, docs) => {
-      if (err) return res.json({ success: false, error: err });
-      return res.json({ success: true, data: docs });
-    });
+    console.log(req.query);
+    if (req.query) {
+        var query = Workout.find(req.query);
+        query.exec((err, docs) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: docs });
+        });        
+    }
+    else {
+        var query = Workout.find();
+        query.exec((err, docs) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: docs });
+        });
+    }
 });
 
   
 router.post('/', (req, res) => {
-    const Workout = new Workout();
-    // body parser lets us use the req.body
-    const { author, text } = req.body;
-    if (!author || !text) {
+    const newWorkout = new Workout({
+        name: req.body.name,
+        mode: req.body.mode
+    });
+    console.log(JSON.stringify(newWorkout));
+    if (!newWorkout.name || !newWorkout.mode) {
         // we should throw an error. we can do this check on the front end
         return res.json({
             success: false,
-            error: 'You must provide an author and comment'
+            error: 'You must provide a name and mode'
         });
     }
-    comment.author = author;
-    comment.text = text;
-    comment.save(err => {
+    newWorkout.save(err => {
         if (err) return res.json({ success: false, error: err });
         return res.json({ success: true });
     });
