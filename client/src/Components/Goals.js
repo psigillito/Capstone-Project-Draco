f491679fd1c73450c78579cc2c1b97e9c79a55b5
Chris Capps <capps.christopher@gmail.com>
2018-07-14 07:33:49 -0400

    Updates to the goal quiz

--- client/src/Components/Goals.js
+++ client/src/Components/Goals.js
@@ -2,33 +2,48 @@ import React, {Component} from 'react'
 import * as goals from '../copy/goals.json'
 import * as  logistics from '../copy/logistics.json'
 
-class Goals extends Component{
-
-    render(){
-        return (
-        <div className="goals-quiz">
-            <form action="" method="post" class="goals-quiz">
-                <fieldset>
-                    <legend>{goals.goals.question}</legend>
-                    <input type="radio" name="goals-radio" id="goals-radio-0" required></input>
-                    <label for="goals-radio-0">{goals.goals.responses[0].text}</label>
-                    <br/>
-                    <input type="radio" name="goals-radio" id="goals-radio-1" required></input>
-                    <label for="goals-radio-1">{goals.goals.responses[1].text}</label>
-                    <br/>
-                    <input type="radio" name="goals-radio" id="goals-radio-2" required></input>
-                    <label for="goals-radio-2">{goals.goals.responses[2].text}</label>
-                    <br/>
-                    <input type="radio" name="goals-radio" id="goals-radio-3" required></input>
-                    <label for="goals-radio-3">{goals.goals.responses[3].text}</label>
-                    <br/>
-                    <input type="radio" name="goals-radio" id="goals-radio-4" required></input>
-                    <label for="goals-radio-4">{goals.goals.responses[4].text}</label>
-                </fieldset>
-            </form>
-        </div>
-        )
+class Goals extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
-}
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      // POST the data to Mongo
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <div class="container-fluid">
+        <div class="row">
+        <form onSubmit={this.handleSubmit}>
+            <div class="form-group">
+                <label>
+                    {goals.goals.question}
+                    <select class="form-control" value={this.state.value} onChange={this.handleChange}>
+                    <option value="{goals.goals.responses[0].value}">{goals.goals.responses[0].text}</option>
+                    <option value="{goals.goals.responses[1].value}">{goals.goals.responses[1].text}</option>
+                    <option value="{goals.goals.responses[2].value}">{goals.goals.responses[2].text}</option>
+                    <option value="{goals.goals.responses[3].value}">{goals.goals.responses[3].text}</option>
+                    <option value="{goals.goals.responses[4].value}">{goals.goals.responses[4].text}</option>
+                    </select>
+                </label>
+            </div>
+            <input class="btn btn-primary" type="submit" value="Submit" />
+        </form>
+        </div>
+      </div>
+        
+      );
+    }
+  }
 
-export default Goals
+  export default Goals
