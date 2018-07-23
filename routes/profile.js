fe5432f4ec520a88867abde742491da8e92bc9fd
Nick DeJarnette <nick.dejarnette@gmail.com>
2018-07-22 23:54:35 -0400

    added passport jwt strategy

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,20 @@
 // user profile information
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-// create or edit user profile
-router.post('/', (req, res) => {
-	// handle input from front end for goals, etc..
+// Import user model
+const User = require('../models/users');
+
+// get current user profile
+// private route
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+	res.json({
+		name: req.user.name,
+		email: req.user.email,
+		username: req.user.username,
+		plans: req.user.trainingPlans
+	});
 });
 
 module.exports = router;
