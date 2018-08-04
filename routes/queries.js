c83db0bf584fc27a1bbcf6b1f45a8f80e315247f
Chris Capps <capps.christopher@gmail.com>
2018-08-04 14:39:59 -0400

    Added get queries to get entities by Id

--- routes/queries.js
+++ routes/queries.js
@@ -3,17 +3,33 @@ const Workout = require('../models/workouts');
 const TrainingPlan = require('../models/trainingPlans');
 
 /*
+ * This function gets a user object by specified userId
+ * @parameter {object} userId, the id string specifying the user to fetch
+ * @parameter {object} res, the response object from the route
+**/
+var _getUser = function(userId, res) {
+	User.findById(userId, (error, doc) => {
+		if (error) {
+			console.log(error);
+		}
+		res.json(doc);
+	});
+}
+
+/*
  * This function saves a new User object (newUser)
- * Then calls the callback function
+ * @parameter {object} newUser, contains the fields that define a new user
+ * @parameter {function} callback, function to execute when the save "promise" returns
 **/
 var _createUser = function(newUser, callback) {
 	newUser.save().then(callback);
 }
 
 /*
- * This function updates a user specified by userId
- * with the object defined in updateObj
- * @parameter res {object} OPTIONAL the response object from the route
+ * This function updates a user specified by userId with the information contained in updateObj
+ * @parameter {string} userId, the id string specifying the user to update
+ * @parameter {object} updateObj, object specifying that fields to be updated
+ * @parameter {object} res OPTIONAL the response object from the route
 **/
 var _updateUser = function(userId, updateObj, res) {
 	User.findByIdAndUpdate(userId, updateObj, (error, doc) => {
@@ -28,19 +44,38 @@ var _updateUser = function(userId, updateObj, res) {
 
 /*
  * This function deletes a user specified by userId
+ * @parameter {string} userId, the id string specifying the user to delete
+ * @parameter {object} OPTIONAL res, the response object from the Express route
 **/
-var _deleteUser = function(userId) {
+var _deleteUser = function(userId, res) {
 	User.findByIdAndRemove(userId, (error, doc) => {
 		if (error) {
 			console.log(error);
-			console.log(doc);
+		}
+		if (typeof res !== "undefined") {
+			res.json({action: "deleted", entityId: userId});
 		}
 	});
 }
 
 /*
+ * This function gets a trainingPlan object by specified trainingPlanId
+ * @parameter {object} trainingPlanId, the id string specifying the trainingPlan to fetch
+ * @parameter {object} res, the response object from the route
+**/
+var _getTrainingPlan = function(trainingPlanId, res) {
+	TrainingPlan.findById(trainingPlanId, (error, doc) => {
+		if (error) {
+			console.log(error);
+		}
+		res.json(doc);
+	});
+}
+
+/*
  * This function saves a new TrainingPlan object (newTrainingPlan)
- * Then calls the callback function
+ * @parameter {object} newTrainingPlan, contains the fields that define a new trainingPlan
+ * @parameter {function} callback, function to execute when the save "promise" returns
 **/
 var _createTrainingPlan = function(newTrainingPlan, callback) {
 	newTrainingPlan.save().then(callback);
@@ -48,7 +83,9 @@ var _createTrainingPlan = function(newTrainingPlan, callback) {
 
 /*
  * This function updates a trainingPlan specified by trainingPlanId
- * with the object defined in updateObj
+ * @parameter {string} trainingPlanId, the id string specifying the trainingPlan to update
+ * @parameter {object} updateObj, object specifying that fields to be updated
+ * @parameter {object} res OPTIONAL the response object from the route
 **/
 var _updateTrainingPlan = function(trainingPlanId, updateObj, res) {
 	TrainingPlan.findByIdAndUpdate(trainingPlanId, updateObj, (error, doc) => {
@@ -63,19 +100,38 @@ var _updateTrainingPlan = function(trainingPlanId, updateObj, res) {
 
 /*
  * This function deletes a trainingPlan specified by trainingPlanId
+ * @parameter {string} trainingPlanId, the id string specifying the trainingPlan to delete
+ * @parameter {object} OPTIONAL res, the response object from the Express route
 **/
-var _deleteTrainingPlan = function(trainingPlanId) {
+var _deleteTrainingPlan = function(trainingPlanId, res) {
 	TrainingPlan.findByIdAndRemove(trainingPlanId, (error, doc) => {
 		if (error) {
 			console.log(error);
-			console.log(doc);
+		}
+		if (typeof res !== "undefined") {
+			res.json({action: "deleted", entityId: trainingPlanId});
 		}
 	});
 }
 
 /*
+ * This function gets a workout object by specified workoutId
+ * @parameter {object} workoutId, the id string specifying the workout to fetch
+ * @parameter {object} res, the response object from the route
+**/
+var _getWorkout = function(workoutId, res) {
+	Workout.findById(workoutId, (error, doc) => {
+		if (error) {
+			console.log(error);
+		}
+		res.json(doc);
+	});
+}
+
+/*
  * This function saves a new Workout object (newWorkout)
- * Then calls the callback function
+ * @parameter {object} newWorkout, contains the fields that define a new workout
+ * @parameter {function} callback, function to execute when the save "promise" returns
 **/
 var _createWorkout = function(newWorkout, callback) {
 	newWorkout.save().then(callback)
@@ -83,19 +139,25 @@ var _createWorkout = function(newWorkout, callback) {
 
 /*
  * This function updates a workout specified by workoutId
- * with the object defined in updateObj
+ * @parameter {string} workoutId, the id string specifying the workout to update
+ * @parameter {object} updateObj, object specifying that fields to be updated
+ * @parameter {object} res OPTIONAL the response object from the route
 **/
-var _updateWorkout = function(workoutId, updateObj) {
+var _updateWorkout = function(workoutId, updateObj, res) {
 	Workout.findByIdAndUpdate(workoutId, updateObj, (error, doc) => {
 		if (error) {
 			console.log(error);
-			console.log(doc);
+		}
+		if (typeof res !== "undefined") {
+			res.json(doc);
 		}
 	});
 }
 
 /*
  * This function deletes a workout specified by workoutId
+ * @parameter {string} workoutId, the id string specifying the workout to delete
+ * @parameter {object} OPTIONAL res, the response object from the Express route
 **/
 var _deleteWorkout = function(workoutId, res) {
 	Workout.findByIdAndRemove(workoutId, (error, doc) => {
@@ -106,39 +168,7 @@ var _deleteWorkout = function(workoutId, res) {
 	});
 }
 
-/*
- * This function returns all the workouts from a specified trainingPlan
- * @parameter {string} trainingPlanId 
- * @returns {array} containing ids of the workouts
-**/
-var _findWorkoutsFromTrainingPlan = function(trainingPlanId) {
-	// query workouts to get an array of workouts
-	var workouts = Workout.find({trainingPlan: trainingPlanId}, {_id:1}, (err, workout) => {
-		if (err) {
-			console.log(err);
-			console.log(workout);
-		}
-		return workout;
-	});
-	return workouts;
-}
 
-/*
- * This function returns all the workouts from a specified user
- * @parameter {string} userId 
- * @returns {array} containing ids of the workouts
-**/
-var _findWorkoutsFromUser = function(userId) {
-	// query workouts to get an array of workouts
-	Workout.find({user: userId}, '_id', (err, workout) => {
-		if (err) {
-			console.log(err);
-			console.log(workout);
-		}
-		return workout;
-	});
-
-}
 
 /*
  * This function returns all the trainingPlans from a specified user
@@ -172,19 +202,6 @@ var _findNumTrainingPlansFromUser = function(userId) {
 	});
 }
 
-/*
- * This function returns the number of workouts from a specified user
- * @parameter {string} userId 
- * @returns {number} containing the total number of the workouts the user has created
-**/
-var _findNumWorkoutsFromUser = function(userId, res) {
-	User.findById(userId, { numWorkouts: 1, _id:0 }, (err, result) => {
-		if (err) {
-			console.log(err);
-		}
-		res.json(result);
-	});
-}
 
 /*
  * This function updates the number of workouts a specified user has created
@@ -234,13 +251,63 @@ var _deleteWorkoutFromTrainingPlan = function(trainingPlanId, workoutId, res) {
 	});
 }
 
+/*
+ * This function returns all the workouts from a specified trainingPlan
+ * @parameter {string} trainingPlanId 
+ * @returns {array} containing ids of the workouts
+**/
+var _findWorkoutsFromTrainingPlan = function(trainingPlanId) {
+	// query workouts to get an array of workouts
+	var workouts = Workout.find({trainingPlan: trainingPlanId}, {_id:1}, (err, workout) => {
+		if (err) {
+			console.log(err);
+			console.log(workout);
+		}
+		return workout;
+	});
+	return workouts;
+}
+
+/*
+ * This function returns all the workouts from a specified user
+ * @parameter {string} userId 
+ * @returns {array} containing ids of the workouts
+**/
+var _findWorkoutsFromUser = function(userId) {
+	// query workouts to get an array of workouts
+	Workout.find({user: userId}, '_id', (err, workout) => {
+		if (err) {
+			console.log(err);
+			console.log(workout);
+		}
+		return workout;
+	});
+}
+
+/*
+ * This function returns the number of workouts from a specified user
+ * @parameter {string} userId 
+ * @returns {number} containing the total number of the workouts the user has created
+**/
+var _findNumWorkoutsFromUser = function(userId, res) {
+	User.findById(userId, { numWorkouts: 1, _id:0 }, (err, result) => {
+		if (err) {
+			console.log(err);
+		}
+		res.json(result);
+	});
+}
+
 queries = {
+	getUser: _getUser,
 	createUser: _createUser,
 	updateUser: _updateUser,
 	deleteUser: _deleteUser,
+	getWorkout: _getWorkout,
 	createWorkout: _createWorkout,
 	updateWorkout: _updateWorkout,
 	deleteWorkout: _deleteWorkout,
+	_getTrainingPlan: _getTrainingPlan,
 	createTrainingPlan: _createTrainingPlan,
 	updateTrainingPlan: _updateTrainingPlan,
 	deleteTrainingPlan: _deleteTrainingPlan,
