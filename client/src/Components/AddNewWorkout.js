03a7522fd40270816d847cf85432e48ec2d245fc
Philip Sigillito <psigillito@gmail.com>
2018-08-04 16:05:54 -0500

    cleaned up add workout component

--- client/src/Components/AddNewWorkout.js
+++ client/src/Components/AddNewWorkout.js
@@ -36,6 +36,7 @@ class AddNewWorkout extends Component {
         this.removeExercise = this.removeExercise.bind(this);
     }
 
+    //Adds exercise to the allExercises array and clears component state elements for next exercise 
     saveExercise() {
         createdExercise['name'] = this.state.exerciseName;
         createdExercise['sets'] = this.state.sets;
@@ -59,8 +60,8 @@ class AddNewWorkout extends Component {
         })
     }
 
+    //saves workout to database 
     submitWorkout() {
- 
         const newWorkout = {
             name: this.state.name,
             mode: this.state.mode,
@@ -86,6 +87,7 @@ class AddNewWorkout extends Component {
             .catch(err => console.log(err));
     }
 
+    //sets available fields for running or weight training
     onChange(e) {
         this.setState({ [e.target.name]: e.target.value })
 
@@ -93,18 +95,17 @@ class AddNewWorkout extends Component {
           this.setState({ allExercises: [] })
         }
         
-        //if running exercise selected
+        //if running exercise selected 
         if(e.target.value == 'Running'){
-
+          //if user has a strava account associated 
           if(this.props.stravaToken.length > 1)
           {
-            //get user Id and update routes state 
             this.getAthleteId()
           }
-          
         }
     }
 
+    //Updates week days for workout 
     handleDayChange(e) {
         if(this.state.exerciseDays.includes(e.target.value)) {
             const index = this.state.exerciseDays.indexOf(e.target.value);
@@ -114,8 +115,8 @@ class AddNewWorkout extends Component {
         }
     }
 
+    //gets athlete id from strava then gets list of routes and saves it to redux state
     getAthleteId(){
-
       fetch('https://www.strava.com/api/v3/athlete?access_token='+this.props.stravaToken).then((results) => results.json())
       .then( (results) => {
         
@@ -132,12 +133,12 @@ class AddNewWorkout extends Component {
             var tempRoute = { name: results[i].name, id: results[i].id}
             athleteRoutes.push(tempRoute)
           }
-
           this.props.setAthleteRoutes(athleteRoutes)
         })
       })
     }
 
+    //if selected to use strava
     handleStravaChecked(e){
       
       //if closing the strava options, set currentRoute state to -1 and selectedRoute to -1
@@ -156,7 +157,7 @@ class AddNewWorkout extends Component {
       })
     }
 
-    //sets new polyline based 
+    //sets new polyline that is passed to google map based on selected route
     handleRouteChange(e)
     {
       var id = e.nativeEvent.target.value;
@@ -184,7 +185,6 @@ class AddNewWorkout extends Component {
       }
     }
 
-
     removeExercise(e){
       var name = e.nativeEvent.target.value;
 
@@ -194,7 +194,6 @@ class AddNewWorkout extends Component {
     }
 
     render() {
-
       let hiddenOptions = this.state.stravaChecked ? '': 'hidden';
       const hasStravaToken = (this.props.stravaToken.length > 1);
       let useStrava;
@@ -233,40 +232,23 @@ class AddNewWorkout extends Component {
           </div>
 
           <div class="modal-body ">
-
             <label for='name'><b>Workout Name:</b></label>
             <div className="form-group">
-              <input 
-                type="text" 
-                className="form-control form-control-lg"
-                placeholder="Workout Name" 
-                name="name" 
-                value={this.state.name}
-                onChange={this.onChange}
-              />
+              <input type="text" className="form-control form-control-lg" placeholder="Workout Name" name="name"value={this.state.name} 
+                     onChange={this.onChange}/>
             </div>
 
             <label for='mode'><b>Workout Type:</b></label>
             <div className="form-group">
               <div className="form-check form-check-inline">
-                  <input className="form-check-input" 
-                  type="radio" 
-                  name="mode" 
-                  id="weightTraining" 
-                  value='Weight Training'
-                  onChange={this.onChange}
-                  />
+                  <input className="form-check-input" type="radio" name="mode" id="weightTraining" value='Weight Training'
+                         onChange={this.onChange}/>
                   <label className="form-check-label" for="inlineRadio1">Weight Training</label>
               </div>
 
               <div className="form-check form-check-inline">
-                  <input className="form-check-input" 
-                  type="radio" 
-                  name="mode" 
-                  id="running" 
-                  value="Running"
-                  onChange={this.onChange}
-                  />
+                  <input className="form-check-input" type="radio" name="mode" id="running" value="Running"
+                         onChange={this.onChange}/>
                   <label className="form-check-label" for="inlineRadio2">Running</label>
               </div>
             </div>
@@ -278,8 +260,7 @@ class AddNewWorkout extends Component {
                   <input className="form-check-input" type="checkbox" name="daysOfWeek" onChange={this.handleDayChange} value={index}/>
                   <label className="form-check-label" for="inlineCheckbox1">{weekDay}</label>
                 </div>
-                )
-              }
+              )}
             </div>
 
             <label for='name'><b>Training Plan: </b>(each workout must be part of a training plan)</label>
@@ -292,141 +273,120 @@ class AddNewWorkout extends Component {
               </select>
             </div>
 
-          {/* Add exercise mode alert */}
-          {this.state.mode === '' && 
-            <div>
-              <div class="alert alert-primary" role="alert">
-                You must select a workout mode to add exercises!
-              </div>
-            </div>
-          }
-
+            {/* Add exercise mode alert */}
+            {this.state.mode === '' && 
+                <div class="alert alert-primary" role="alert">You must select a workout mode to add exercises!</div>
+            }
            
             {/* Exercises - Weight training */}
             {this.state.mode === 'Weight Training' && this.state.mode != '' &&
-            <fieldset>
-            <div>
-              <div class="modal-header">
-                <h5>Add Exercise</h5>
-              </div>
-              <div className="form-group">
-                <input 
-                  type="text" 
-                  className="form-control form-control-lg"
-                  placeholder="Exercise Name" 
-                  name="exerciseName" 
-                  value={this.state.exerciseName}
-                  onChange={this.onChange}
-                />
-              </div>
-              <form>
-                <div class="form-row">
-                  <div class="col">
-                    <input type="number" class="form-control" name="sets" value={this.state.sets} onChange={this.onChange} placeholder="Sets"/>
-                  </div>
-                  <div class="col">
-                    <input type="number" class="form-control" name="reps" value={this.state.reps} onChange={this.onChange} placeholder="Reps"/>
-                  </div>
-                  <div class="col">
-                    <input type="number" class="form-control" name="weight" value={this.state.weight} onChange={this.onChange} placeholder="Weight"/>
-                  </div>
-                  <div class="col">
-                   <select id="inputState" name="unit" class="form-control" onChange={this.onChange}>
-                      <option name="unit" value="lbs">Lbs</option>
-                      <option name="unit" value="kg">Kg</option>
-                   </select>
-                  </div>
+              <div>
+                <div class="modal-header">
+                  <h5>Add Exercise</h5>
                 </div>
-              </form>
-              <br />
-            <div>
-                <button type="button" onClick={() => this.saveExercise()} class="btn btn-primary btn-block">
-                  Add Exercise To Workout
-                </button>
-            </div>
-            </div>
-            </fieldset>
-          }
-
-          {/* Exercises - Running */}
-          {this.state.mode === 'Running' && this.state.mode != '' && 
-          <div>
-          <div class="modal-header">
-            <h5>Add Exercises</h5>
-          </div>
-          {useStrava}
-          <br/>
-          <div className="form-group">
-            <input 
-              type="text" 
-              className="form-control form-control-lg"
-              placeholder="Exercise Name" 
-              name="exerciseName" 
-              value={this.state.exerciseName}
-              onChange={this.onChange}
-            />
-            </div>
-          <label for='duration'><b>Duration:</b></label>
-              <fieldset disabled={this.state.stravaChecked}>
+                <div className="form-group">
+                  <input 
+                    type="text" className="form-control form-control-lg" placeholder="Exercise Name" name="exerciseName" 
+                    value={this.state.exerciseName} onChange={this.onChange}
+                  />
+                </div>
                 <form>
                   <div class="form-row">
                     <div class="col">
-                      <input type="number" id="inputDistance" class="form-control" name="distance" value={this.state.distance} onChange={this.onChange} placeholder="Distance"/>
+                      <input type="number" class="form-control" name="sets" value={this.state.sets} onChange={this.onChange} placeholder="Sets"/>
                     </div>
                     <div class="col">
-                    <select id="inputState" name="distanceUnit" class="form-control" onChange={this.onChange}>
-                        <option selected name="distanceUnit" value="mi">Mi</option>
-                        <option name="distanceUnit" value="km">Km</option>
+                      <input type="number" class="form-control" name="reps" value={this.state.reps} onChange={this.onChange} placeholder="Reps"/>
+                    </div>
+                    <div class="col">
+                      <input type="number" class="form-control" name="weight" value={this.state.weight} onChange={this.onChange} placeholder="Weight"/>
+                    </div>
+                    <div class="col">
+                    <select id="inputState" name="unit" class="form-control" onChange={this.onChange}>
+                        <option name="unit" value="lbs">Lbs</option>
+                        <option name="unit" value="kg">Kg</option>
                     </select>
                     </div>
                   </div>
                 </form>
-              </fieldset>
-              <br />
-              <div>
-                  <button type="button" onClick={() => this.saveExercise()} class="btn btn-primary btn-block">
-                    Add Exercise To Workout
-                  </button>
+                <br/>
+                <div>
+                  <button type="button" onClick={() => this.saveExercise()} class="btn btn-primary btn-block">Add Exercise To Workout</button>
+                </div>
               </div>
-          </div>
-        }       
-          {/*Display Pending Workouts*/}
-          <br/>
-          <div>
-            <h4>Workout Summary</h4>
-            <table className="table"> 
-              <thead>
-                <th>Exercise Name</th>
-                <th>{ (this.state.mode == 'Running') ? 'Distance' : 'Weight' }</th>
-                <th>Units</th>
-                <th>{ (this.state.mode == 'Running') ? '' : 'Reps' }</th>
-                <th>{ (this.state.mode == 'Running') ? '' : 'Sets' }</th>
-                <th></th>
-              </thead>
-              <tbody>
-                {this.state.allExercises.map( (exercise) =>
-                  <tr>
-                    <td>{exercise.name}</td> 
-                    <td>{ (this.state.mode == 'Running') ? exercise.distance : exercise.weight }</td>
-                    <td>{ (this.state.mode == 'Running') ? exercise.distanceUnit : exercise.unit }</td>
-                    <td>{ (this.state.mode == 'Running') ? '' : exercise.reps }</td>
-                    <td>{ (this.state.mode == 'Running') ? '' : exercise.sets }</td>
-                    <td><button className="btn btn-danger" value={exercise.name} onClick={this.removeExercise}>X</button></td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
-          <br/>
-      </div>
+            }
 
-
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal" data-target="#addWorkout">Close</button>
-        <button type="button" onClick={() => this.submitWorkout()} data-toggle="modal" data-target="#addExercise" class="btn btn-success">Save Workout</button>
-      </div>
-    </div>
-    )}
+            {/* Exercises - Running */}
+            {this.state.mode === 'Running' && this.state.mode != '' && 
+              <div>
+                <div class="modal-header">
+                  <h5>Add Exercises</h5>
+                </div>
+                {useStrava}
+                <br/>
+                <div className="form-group">
+                  <input type="text" className="form-control form-control-lg" placeholder="Exercise Name" name="exerciseName" 
+                        value={this.state.exerciseName} onChange={this.onChange}
+                  />
+                </div>
+                <label for='duration'><b>Duration:</b></label>
+                <fieldset disabled={this.state.stravaChecked}>
+                  <form>
+                    <div class="form-row">
+                      <div class="col">
+                        <input type="number" id="inputDistance" class="form-control" name="distance" value={this.state.distance} onChange={this.onChange} placeholder="Distance"/>
+                      </div>
+                      <div class="col">
+                        <select id="inputState" name="distanceUnit" class="form-control" onChange={this.onChange}>
+                          <option selected name="distanceUnit" value="mi">Mi</option>
+                          <option name="distanceUnit" value="km">Km</option>
+                        </select>
+                      </div>
+                    </div>
+                  </form>
+                </fieldset>
+                <br/>
+                <div>
+                  <button type="button" onClick={() => this.saveExercise()} class="btn btn-primary btn-block">Add Exercise To Workout</button>
+                </div>
+              </div>
+            }       
+            <br/>
+            <div>
+              <h4>Workout Summary</h4>
+              <table className="table"> 
+                <thead>
+                  <th>Exercise Name</th>
+                  <th>{ (this.state.mode == 'Running') ? 'Distance' : 'Weight' }</th>
+                  <th>Units</th>
+                  <th>{ (this.state.mode == 'Running') ? '' : 'Reps' }</th>
+                  <th>{ (this.state.mode == 'Running') ? '' : 'Sets' }</th>
+                  <th></th>
+                </thead>
+                <tbody>
+                  {this.state.allExercises.map( (exercise) =>
+                    <tr>
+                      <td>{exercise.name}</td> 
+                      <td>{ (this.state.mode == 'Running') ? exercise.distance : exercise.weight }</td>
+                      <td>{ (this.state.mode == 'Running') ? exercise.distanceUnit : exercise.unit }</td>
+                      <td>{ (this.state.mode == 'Running') ? '' : exercise.reps }</td>
+                      <td>{ (this.state.mode == 'Running') ? '' : exercise.sets }</td>
+                      <td><button className="btn btn-danger" value={exercise.name} onClick={this.removeExercise}>X</button></td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <br/>
+          </div>
+      
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" data-target="#addWorkout">Close</button>
+            <button type="button" onClick={() => this.submitWorkout()} data-toggle="modal" data-target="#addExercise" class="btn btn-success">Save Workout</button>
+          </div>
+        </div>
+      )
+    }
   }
 
   const mapStateToProps = function(state) {
